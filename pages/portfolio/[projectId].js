a3c9908e4af472f74b2ea3ae0fb857ba7f96730f
Rki0 <pky00823@gmail.com>
2023-10-20 22:20:18 +0900

    routing deeply

--- pages/portfolio/[projectId].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-function Detailpage() {
-  const router = useRouter();
-
-  return <div>Detail about {router.query.projectId}</div>;
-}
-
-export default Detailpage;
