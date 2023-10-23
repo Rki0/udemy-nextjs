3b60d950abdf6deb05200933e2f81dce7932ad6f
Rki0 <pky00823@gmail.com>
2023-10-23 14:45:05 +0900

    usage of SSR,SSG,ISR,CSR

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,11 @@
-import { getAllEvents } from "@/dummy-data";
-import EventList from "@/components/events/event-list";
-import EventSearch from "@/components/events/event-search";
 import { useRouter } from "next/router";
 
-function AllEventsPage() {
-  const events = getAllEvents();
+import EventList from "@/components/events/event-list";
+import EventSearch from "@/components/events/event-search";
+import { getAllEvents } from "@/helpers/api-utils";
+
+function AllEventsPage(props) {
+  const { events } = props;
   const router = useRouter();
 
   const findEventsHandler = (year, month) => {
@@ -19,11 +20,22 @@ function AllEventsPage() {
   }
 
   return (
-    <div>
+    <>
       <EventSearch onSearch={findEventsHandler} />
       <EventList items={events} />
-    </div>
+    </>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default AllEventsPage;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
-import { getFeaturedEvents } from "@/dummy-data";
 import EventList from "@/components/events/event-list";
+import { getFeaturedEvents } from "@/helpers/api-utils";
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
+  const { featuredEvents } = props;
 
   return (
     <div>
       <EventList items={featuredEvents} />
     </div>
   );
+}
+
+// SEO가 중요한 페이지이다. 로그인이 필요한 페이지가 이니다. 모든 요청에 대해 재실행할 필요가 없다.
+// 따라서 getStaticProps() 사용.
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      featuredEvents,
+    },
+    revalidate: 1800, // 이를 추가하지 않으면 페이지를 다시 build해서 배포해야 업데이트된 상태를 얻을 수 있게된다. 30분에 한번씩 regenerate한다.
+  };
 }
 
 export default HomePage;
